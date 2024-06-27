52f2eb0893a74080ef2211d15a4327d0fce36fc1
priyankalad2012 <153911832+priyankalad2012@users.noreply.github.com>
2024-06-27 09:55:18 +0530

    Add files via upload

---
+++ MAven_Learning/src/test/java/com/AccountTest.java
@@ -0,0 +1,24 @@
+package com;
+
+import org.testng.annotations.Test;
+
+public class AccountTest {
+	
+	@Test
+	public void savingAccount()
+	{
+		System.out.println("running saving account");
+	}
+	
+	@Test
+	public void currentAccount()
+	{
+		System.out.println("running current account");
+	}
+	
+	@Test
+	public void laonAccount()
+	{
+		System.out.println("Running loan account");
+	}
+}
---
+++ MAven_Learning/src/test/java/com/MyTest.java
@@ -0,0 +1,24 @@
+package com;
+
+import org.testng.annotations.Test;
+
+public class MyTest {
+	
+	@Test
+	public void test()
+	{
+		System.out.println("Runing my test");
+	}
+	
+	@Test
+	public void alpha()
+	{
+		System.out.println("runing alpha test");
+	}
+	
+	@Test
+	public void beta()
+	{
+		System.out.println("Running beta test");
+	}
+}
